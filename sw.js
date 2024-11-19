570e6da737060a36c8ff47eb3e10ecb7c56aa52e
githubjllik <githubjllik@gmail.com>
2024-11-19 17:02:26 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,44 @@
+// sw.js
+const routes = {
+    '/home': '/index.html',
+    '/social-media': '/pg2.html',
+    '/streaming': '/pg3.html', 
+    '/learn': '/pg4.html',
+    '/ai': '/pg5.html',
+    '/edit': '/pg6.html',
+    '/develop': '/pg7.html',
+    '/e-services': '/pg8.html',
+    '/explore': '/pg9.html',
+    '/download': '/pg10.html',
+    '/devices': '/pg11.html',
+    '/search': '/pg12.html',
+    '/darkweb': '/pg13.html',
+    '/discover': '/pg14.html',
+    '/about': '/pg15.html',
+    '/new': '/nouveaux.html',
+    '/search-results': '/search-results.html'
+};
+
 self.addEventListener('install', (event) => {
     self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
     event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+    const url = new URL(event.request.url);
+    const path = url.pathname;
+
+    if (routes[path]) {
+        event.respondWith(
+            fetch(new Request(routes[path], {
+                headers: event.request.headers,
+                method: event.request.method
+            }))
+        );
+    } else {
+        event.respondWith(fetch(event.request));
+    }
 });
--- urlDisplay.js
+++ urlDisplay.js
@@ -1,33 +1,32 @@
+// urlDisplay.js
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js')
-        .catch(error => {
-            console.error('Service Worker registration failed:', error);
-        });
+    navigator.serviceWorker.register('/sw.js').then(() => {
+        const pageNames = {
+            'index.html': 'home',
+            'pg2.html': 'social-media',
+            'pg3.html': 'streaming',
+            'pg4.html': 'learn',
+            'pg5.html': 'ai',
+            'pg6.html': 'edit',
+            'pg7.html': 'develop',
+            'pg8.html': 'e-services',
+            'pg9.html': 'explore',
+            'pg10.html': 'download',
+            'pg11.html': 'devices',
+            'pg12.html': 'search',
+            'pg13.html': 'darkweb',
+            'pg14.html': 'discover',
+            'pg15.html': 'about',
+            'nouveaux.html': 'new',
+            'search-results.html': 'search-results'
+        };
+
+        const currentPage = window.location.pathname.split('/').pop();
+        
+        if (pageNames[currentPage]) {
+            history.replaceState(null, '', '/' + pageNames[currentPage]);
+        }
+    }).catch(error => {
+        console.error('Service Worker registration failed:', error);
+    });
 }
-
-const pageNames = {
-    'index.html': 'home',
-    'pg2.html': 'social-media',
-    'pg3.html': 'streaming',
-    'pg4.html': 'learn',
-    'pg5.html': 'ai',
-    'pg6.html': 'edit',
-    'pg7.html': 'develop',
-    'pg8.html': 'e-services',
-    'pg9.html': 'explore',
-    'pg10.html': 'download',
-    'pg11.html': 'devices',
-    'pg12.html': 'search',
-    'pg13.html': 'darkweb',
-    'pg14.html': 'discover',
-    'pg15.html': 'about',
-    'nouveaux.html': 'new',
-    'search-results.html': 'search-results'
-};
-
-document.addEventListener('DOMContentLoaded', () => {
-    const currentPage = window.location.pathname.split('/').pop();
-    if (pageNames[currentPage]) {
-        window.history.replaceState(null, '', '/' + pageNames[currentPage]);
-    }
-});
